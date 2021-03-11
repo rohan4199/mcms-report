5d4a5bddcb777198ecb0e480cb7f74dedb7e65af
Emily Shaffer <emilyshaffer@google.com>
2021-03-10 18:10:29 -0800

    reference-transaction: look for hooks in config
    By using the hook.h library, reference-transaction hooks can be
    specified in the config instead.
    
    The expected output of the test is not fully updated to reflect the
    absolute path of the hook called because the 'update' hook has not yet
    been converted to use hook.h.
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1416-ref-transaction-hooks.sh
+++ t/t1416-ref-transaction-hooks.sh
@@ -122,11 +122,11 @@ test_expect_success 'interleaving hook calls succeed' '
 
 	cat >expect <<-EOF &&
 		hooks/update refs/tags/PRE $ZERO_OID $PRE_OID
-		hooks/reference-transaction prepared
-		hooks/reference-transaction committed
+		$(pwd)/target-repo.git/hooks/reference-transaction prepared
+		$(pwd)/target-repo.git/hooks/reference-transaction committed
 		hooks/update refs/tags/POST $ZERO_OID $POST_OID
-		hooks/reference-transaction prepared
-		hooks/reference-transaction committed
+		$(pwd)/target-repo.git/hooks/reference-transaction prepared
+		$(pwd)/target-repo.git/hooks/reference-transaction committed
 	EOF
 
 	git push ./target-repo.git PRE POST &&
