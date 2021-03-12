317fa63277bb04e95048b657b10e8a60ddb85961
Han-Wen Nienhuys <hanwen@google.com>
2021-03-12 20:19:53 +0000

    reftable: rest of library
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/helper/test-reftable.c
+++ t/helper/test-reftable.c
@@ -5,8 +5,11 @@ int cmd__reftable(int argc, const char **argv)
 {
 	basics_test_main(argc, argv);
 	block_test_main(argc, argv);
+	merged_test_main(argc, argv);
 	record_test_main(argc, argv);
+	refname_test_main(argc, argv);
 	reftable_test_main(argc, argv);
+	stack_test_main(argc, argv);
 	tree_test_main(argc, argv);
 	return 0;
 }
