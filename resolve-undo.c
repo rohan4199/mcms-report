8aaaee579060581da5ea152ee7231d683c2caf53
Derrick Stolee <dstolee@microsoft.com>
2021-03-16 21:17:12 +0000

    resolve-undo: ensure full index
    Before iterating over all cache entries, ensure that a sparse index is
    expanded to a full index to avoid unexpected behavior.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- resolve-undo.c
+++ resolve-undo.c
@@ -172,6 +172,7 @@ void unmerge_marked_index(struct index_state *istate)
 	if (!istate->resolve_undo)
 		return;
 
+	ensure_full_index(istate);
 	for (i = 0; i < istate->cache_nr; i++) {
 		const struct cache_entry *ce = istate->cache[i];
 		if (ce->ce_flags & CE_MATCHED)
@@ -186,6 +187,7 @@ void unmerge_index(struct index_state *istate, const struct pathspec *pathspec)
 	if (!istate->resolve_undo)
 		return;
 
+	ensure_full_index(istate);
 	for (i = 0; i < istate->cache_nr; i++) {
 		const struct cache_entry *ce = istate->cache[i];
 		if (!ce_path_match(istate, ce, pathspec, NULL))
