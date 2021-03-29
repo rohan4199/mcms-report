6de4763f6ad9a051a3269ac145672e5190275c8b
lilinchao <lilinchao@oschina.cn>
2021-03-29 10:19:04 +0000

    builtin/clone.c: add --reject-shallow option
    In some scenarios, users may want more history than the repository
    offered for cloning, which happens to be a shallow repository, can
    give them. But because users don't know it is a shallow repository
    until they download it to local, users should have the option to
    refuse to clone this kind of repository, and may want to exit the
    process immediately without creating any unnecessary files.
    
    Althought there is an option '--depth=x' for users to decide how
    deep history they can fetch, but as the unshallow cloning's depth
    is INFINITY, we can't know exactly the minimun 'x' value that can
    satisfy the minimum integrity, so we can't pass 'x' value to --depth,
    and expect this can obtain a complete history of a repository.
    
    In other scenarios, if we have an API that allow us to import external
    repository, and then perform various operations on the repo.
    But if the imported is a shallow one(which is actually possible), it
    will affect the subsequent operations. So we can choose to refuse to
    clone, and let's just import a normal repository.
    
    This patch offers a new option '--reject-shallow' that can reject to
    clone a shallow repository.
    
    Signed-off-by: lilinchao <lilinchao@oschina.cn>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t5611-clone-config.sh
+++ t/t5611-clone-config.sh
@@ -95,6 +95,38 @@ test_expect_success 'clone -c remote.<remote>.fetch=<refspec> --origin=<name>' '
 	test_cmp expect actual
 '
 
+test_expect_success 'clone.rejectshallow=true should reject cloning' '
+	rm -rf child &&
+	git clone --depth=1 --no-local . child &&
+	test_must_fail git -c clone.rejectshallow=true clone --no-local child out 2>err &&
+	test_i18ngrep -e "source repository is shallow, reject to clone." err
+'
+
+test_expect_success 'clone.rejectshallow=false should succeed' '
+	rm -rf child out &&
+	git clone --depth=1 --no-local . child &&
+	git -c clone.rejectshallow=false clone --no-local child out
+'
+
+test_expect_success 'clone.rejectshallow=true should succeed with normal repo' '
+	rm -rf child out &&
+	git clone --no-local . child &&
+	git -c clone.rejectshallow=true clone --no-local child out
+'
+
+test_expect_success 'option --reject-shallow override clone.rejectshallow' '
+	rm -rf child out &&
+	git clone --depth=1 --no-local . child &&
+	test_must_fail git -c clone.rejectshallow=false clone --reject-shallow --no-local child out 2>err &&
+	test_i18ngrep -e "source repository is shallow, reject to clone." err
+'
+
+test_expect_success 'option --no-reject-shallow override clone.rejectshallow' '
+	rm -rf child out &&
+	git clone --depth=1 --no-local . child &&
+	git -c clone.rejectshallow=true clone --no-reject-shallow --no-local child out
+'
+
 test_expect_success MINGW 'clone -c core.hideDotFiles' '
 	test_commit attributes .gitattributes "" &&
 	rm -rf child &&
